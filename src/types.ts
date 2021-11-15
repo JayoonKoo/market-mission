3465cdd6dec54eafbe48248dbfc1d9087c989a15
JayoonKoo <rnwkdbs12@gmail.com>
2021-11-16 00:52:44 +0900

    feat: 로그인 관련 type 추가

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,39 @@
+import { RouterState } from "connected-react-router";
+import { AnyAction, Reducer } from "redux";
+
+export interface RootState {
+  auth: AuthState;
+  router: Reducer<RouterState<unknown>, AnyAction>;
+}
+
 export type AuthState = {
   token: string | null;
   loading: boolean;
   error: Error | null;
+  user?: User;
+};
+
+export type LoginReqType = {
+  email: string;
+  password: string;
+};
+
+export type User = {
+  email: string;
+  displayName: string;
+  profileImg: string;
+};
+
+// {
+// 	"user": {
+// 		"email": "thesecon@gmail.com",
+// 		"displayName": "ParkYoungWoong",
+// 		"profileImg": "https://storage.googleapis.com/heropy-api/vAKjlJ-Gx5v163442.png"
+// 	},
+// 	"accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjlQS3I...(생략)"
+// }
+
+export type LoginResType = {
+  user: User;
+  accessToken: string;
 };
