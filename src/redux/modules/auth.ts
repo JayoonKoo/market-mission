b851a6dc936fca7ec27662691830c2eac679d9b9
JayoonKoo <rnwkdbs12@gmail.com>
2021-11-15 17:45:20 +0900

    feat: auth reducer 생성

---
+++ src/redux/modules/auth.ts
@@ -0,0 +1,41 @@
+import { createActions, handleActions } from "redux-actions";
+import { AuthState } from "../../types";
+
+const initialState: AuthState = {
+  token: null,
+  loading: false,
+  error: null,
+};
+
+const prefix = "market-mission/auth";
+
+export const { pending, success, fail } = createActions(
+  "PENDING",
+  "SUCCESS",
+  "FAIL",
+  { prefix }
+);
+
+const reducer = handleActions<AuthState, string>(
+  {
+    PENDING: (state) => ({
+      ...state,
+      loading: true,
+      error: null,
+    }),
+    SUCCESS: (state, action) => ({
+      token: action.payload,
+      loading: false,
+      error: null,
+    }),
+    FAIL: (state, action: any) => ({
+      ...state,
+      loading: false,
+      error: action.payload,
+    }),
+  },
+  initialState,
+  { prefix }
+);
+
+export default reducer;
