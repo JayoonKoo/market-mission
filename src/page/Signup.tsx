4abdb8c4307532e8c9a15a4cae8e52f1c8858c7b
JayoonKoo <rnwkdbs12@gmail.com>
2021-11-16 16:51:08 +0900

    feat: signup page 인증 추가

--- src/page/Signup.tsx
+++ src/page/Signup.tsx
@@ -1,7 +1,16 @@
+import { Redirect } from "react-router";
+import SignupContainer from "../containers/SignupContainer";
+import useCurrentUser from "../hooks/useCurrentUser";
+
 export default function Signup() {
+	const user = useCurrentUser()
+	if (!!user) {
+		return <Redirect to="/" />
+	}
+
 	return (
-		<div>
-			회원가입		
+		<div className="signup-container container">
+			<SignupContainer />
 		</div>
 	)
 }
