4189899dcc049f398047a2f007910bce15202818
JayoonKoo <rnwkdbs12@gmail.com>
2021-11-16 16:52:36 +0900

    feat: signup, logtou saga 추가

--- src/redux/modules/auth.ts
+++ src/redux/modules/auth.ts
@@ -3,12 +3,18 @@ import { push } from "connected-react-router";
 import { Action, createActions, handleActions } from "redux-actions";
 import TokenService from "../../services/TokenService";
 import UserService from "../../services/UserService";
-import { AuthState, LoginReqType, LoginResType, User } from "../../types";
+import {
+  AuthState,
+  LoginReqType,
+  LoginResType,
+  SignUpReqType,
+  User,
+} from "../../types";
 
 const initialState: AuthState = {
   token: null,
   loading: false,
-  error: null,
+  errorMsg: null,
   user: undefined,
 };
 
@@ -26,18 +32,18 @@ const reducer = handleActions<AuthState, LoginResType>(
     PENDING: (state) => ({
       ...state,
       loading: true,
-      error: null,
+      errorMsg: null,
     }),
     SUCCESS: (state, action) => ({
       token: action.payload.accessToken,
       loading: false,
-      error: null,
+      errorMsg: null,
       user: action.payload.user,
     }),
     FAIL: (state, action: any) => ({
       ...state,
       loading: false,
-      error: action.payload,
+      errorMsg: action.payload,
     }),
   },
   initialState,
@@ -48,10 +54,11 @@ export default reducer;
 
 // saga
 
-export const { login, logout, auth } = createActions(
+export const { login, logout, auth, signup } = createActions(
   "LOGIN",
   "LOGOUT",
   "AUTH",
+  "SIGNUP",
   { prefix }
 );
 
@@ -66,7 +73,7 @@ function* loginSaga(action: Action<LoginReqType>) {
     yield put(success(loginUser));
     yield put(push("/"));
   } catch (error: any) {
-    const madeError = new Error(error?.response?.data || "UNKWON_ERROR");
+    const madeError = error?.response?.data || "UNKWON_ERROR";
     yield put(fail(madeError));
   }
 }
@@ -75,19 +82,53 @@ function* authConfirmSaga() {
   try {
     yield put(pending());
     const token = TokenService.get();
-    if (token == null) {
+    console.log(token);
+
+    if (token === null) {
       yield put(success({ accessToken: token, user: undefined }));
     } else {
       const currentUser: User = yield call(UserService.auth, token);
       yield put(success({ accessToken: token, user: currentUser }));
     }
   } catch (error: any) {
-    const madeError = new Error(error?.response?.data || "UNKWON_ERROR");
+    const madeError = error?.response?.data || "UNKWON_ERROR";
     yield put(fail(madeError));
+  }
+}
+
+function* signUpSaga(action: Action<SignUpReqType>) {
+  try {
+    yield put(pending());
+    yield call(UserService.signup, action.payload);
+
+    yield put(
+      success({
+        user: undefined,
+        accessToken: null,
+      })
+    );
+    yield put(push("/"));
+  } catch (error: any) {
+    const madeError = error?.response?.data || "UNKWON_ERROR";
+    yield put(fail(madeError));
+  }
+}
+
+function* logoutSaga() {
+  try {
+    yield put(pending());
+    const token = TokenService.get();
+    if (token === null) throw new Error("토큰이 없습니다.");
+    yield call(UserService.logout, token);
+    yield put(success({ user: undefined, accessToken: null }));
+  } catch (error: any) {
+    yield put(fail(error?.message || error?.response?.data || "UNKWON_ERROR"));
   }
 }
 
 export function* authSaga() {
   yield takeEvery(`${prefix}/LOGIN`, loginSaga);
   yield takeEvery(`${prefix}/AUTH`, authConfirmSaga);
+  yield takeEvery(`${prefix}/SIGNUP`, signUpSaga);
+  yield takeEvery(`${prefix}/LOGOUT`, logoutSaga);
 }
